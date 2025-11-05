9ee130947adda05f462550fd39dd6ae16d600410
unknown <Cristianlucero.ala2@gmail.com>
2025-11-05 11:44:30 -0300

    Card Compound Components

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./index.css";
 import Header from "./componentes/Header.tsx";
 import MainLayout from "./pages/MainLayout.tsx";
 import Footer from "./componentes/Footer.tsx";
+import Card from "./componentes/ui/Card.tsx";
 
 
 function App() {
@@ -10,6 +11,31 @@ function App() {
     <>
     <Header />
     <MainLayout />
+    <Card>
+      <Card.Header>
+        CardHeader
+      </Card.Header>
+      
+      <Card.Body>
+        <div>
+          <div>
+            <h3>Auriculares Pro</h3>
+            <p>Cancelación de ruido y bateria de larga duración</p>
+          </div>
+        </div>
+        <img src="https://images.pexels.com/photos/34578398/pexels-photo-34578398.jpeg" 
+        alt="Auriculares"
+        className=""
+        />
+      </Card.Body>
+
+      <Card.Footer>
+        <div>
+          <button>Leer post</button>
+          <button><img src="/src/assets/share.svg" alt="Compartir"/>Compartir</button>
+        </div>
+      </Card.Footer>
+    </Card>
     <Footer />
     </>
   )    
